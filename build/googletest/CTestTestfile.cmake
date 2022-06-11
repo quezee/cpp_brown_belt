a366d9f2b322170eb32ce4eff5ee462b6fa42406
Kirill Kuzmin <quezee@yandex.com>
2022-06-11 17:22:11 +0300

    minor clean up

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quezee/edu/cpp/cpp_brown/final/googletest
-# Build directory: /home/quezee/edu/cpp/cpp_brown/final/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quezee/edu/cpp/cpp_brown/final/googletest/googletest
-# Build directory: /home/quezee/edu/cpp/cpp_brown/final/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
